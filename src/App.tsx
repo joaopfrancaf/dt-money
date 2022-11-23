12f11739d71dea8420353f7d0324f1d7da79f150
João Pedro FF <joaofrancaf@hotmail.com>
2022-11-23 13:38:45 -0300

    first

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/themes/default'
+
+export function App() {
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+
+      <h1>olarr</h1>
+    </ThemeProvider>
+  )
+}
