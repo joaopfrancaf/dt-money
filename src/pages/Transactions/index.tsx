58d8a8e0c3baf09f7de13f9cb146e2a0dd250cfd
joão.frança <joaofrancaf@hotmail.com>
2022-11-23 15:36:58 -0300

    third

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from "../../components/Header/header";
 import { Summary } from "../../components/Summary";
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header/>
             <Summary/>
+
+            <TransactionsContainer>
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td>Desenvolvimento de Site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    $ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td>hamburger</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    -$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>alimentacao</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
+           
     )
 }
